3b6d173be9fbcb73698bdf84ca2cfc666f231f69
Pavel71 <raz_3@mail.ru>
2020-08-29 15:10:10 +0300

    Rendering Edit Profile

--- InstagramApp/Screens/OtherScreens/SettingsViewController.swift
+++ InstagramApp/Screens/OtherScreens/SettingsViewController.swift
@@ -7,25 +7,33 @@
 //
 
 import UIKit
+import SafariServices
 
 
-
-
+struct SettingCellModel {
+  var label   : String
+  var handler : (() -> Void)
+}
 
 class SettingsViewController: UIViewController {
   
-  // MARK: - Section
-  enum Section :Int,CaseIterable {
-    case logOut
-  }
+
   
   // MARK: - Views
   
-  private let tableView : UITableView = {
+  private lazy var  tableView : UITableView = {
     let table = UITableView(frame: .zero, style: .grouped)
     table.backgroundColor = .systemBackground
+    table.delegate        = self
+    table.dataSource      = self
     return table
   }()
+  
+  
+  // MARK: - Data
+  
+  
+  var data : [[SettingCellModel]] = []
   
   
   
@@ -39,23 +47,78 @@ class SettingsViewController: UIViewController {
     
     title = "Settings"
     
-    tableView.delegate   = self
-    tableView.dataSource = self
     setUpViews()
+    configureModels()
   }
+  
+  // MARK: - Configure Models
+  
+  private func configureModels() {
+    
+    data.append(
+      [
+        SettingCellModel.init(label: "Edit Profile", handler: {[weak self] in
+          self?.didTapEditProfile()
+      }),
+        SettingCellModel.init(label: "Invite Friedns", handler: {[weak self] in
+          self?.didTapInviteFriedns()
+        }),
+        SettingCellModel.init(label: "Save Oroginal Post", handler: {[weak self] in
+          self?.saveOriginalPost()
+        })
+      ]
+    )
+    
+    data.append(
+      [
+        SettingCellModel.init(label: "Terms of privacy", handler: {[weak self] in
+          self?.openUrl(.terms)
+        }),
+        SettingCellModel.init(label: "Privacy Policy", handler: {
+          [weak self] in
+          self?.openUrl(.privacy)
+        }),
+        SettingCellModel.init(label: "Help / Feedback", handler: {
+          [weak self] in
+          self?.openUrl(.help)
+        })
+      ]
+    )
+    
+    data.append([
+      SettingCellModel.init(label: "Log out", handler: {[weak self] in
+        self?.logOut()
+      }),
+    ])
+    
+    
+  }
+  
+  
+  
+
+  
+  
+  
 
   
   
   
   
+}
+
+// MARK: - Signals
+extension SettingsViewController {
+  
   
   // MARK: - Log Out
   private func logOut() {
     
+
     let actionSheetController = UIAlertController(title: "Log Out", message: "Are you sure to LogOut", preferredStyle: .actionSheet)
-    
+
     actionSheetController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-    
+
     actionSheetController.addAction(UIAlertAction(title: "Log Out", style: .destructive, handler: { (action) in
       self.authLogOut()
     }))
@@ -90,6 +153,39 @@ class SettingsViewController: UIViewController {
     }
   }
   
+  enum SettingsURLType {
+    case terms,privacy,help
+  }
+  
+  private func openUrl(_ type : SettingsURLType) {
+    let urlString: String
+    switch type {
+    case .help:    urlString = "https://help.instagram.com/581066165581870"
+    case .privacy: urlString = "https://help.instagram.com/196883487377501"
+    case .terms:   urlString = "https://help.instagram.com"
+    
+    }
+    
+    guard let url = URL(string: urlString) else {return}
+    let vc = SFSafariViewController(url: url)
+    present(vc, animated: true, completion: nil)
+  }
+  
+  // MARK: - Edit Profile
+  private func didTapEditProfile() {
+    let vc = EditProfileViewController()
+    let navVc = UINavigationController(rootViewController: vc)
+    present(navVc, animated: true, completion: nil)
+  }
+  // MARK: - Invite Friends
+  private func didTapInviteFriedns() {
+  
+  }
+  
+  // MARK: - Save Priginal Post
+  private func saveOriginalPost() {
+    
+  }
   
 }
 
@@ -105,21 +201,16 @@ extension SettingsViewController {
 // MARK: - Delegate DataSource
 extension SettingsViewController: UITableViewDelegate,UITableViewDataSource {
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    1
+    data[section].count
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    
-    guard
-      let section = Section(rawValue: indexPath.section)
-    else {return UITableViewCell()}
+
     
     let cell = UITableViewCell()
-    
-    switch  section {
-    case .logOut:
-      cell.textLabel?.text = "Log out"
-    }
+    cell.accessoryType   = .disclosureIndicator
+    cell.textLabel?.text = data[indexPath.section][indexPath.row].label
+
     
     return cell
   }
@@ -127,18 +218,14 @@ extension SettingsViewController: UITableViewDelegate,UITableViewDataSource {
   
   
   func numberOfSections(in tableView: UITableView) -> Int {
-    return Section.allCases.count
+    return data.count
   }
   
   func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        guard let section = Section(rawValue: indexPath.section) else {return}
-        
-        switch section {
-        case .logOut:
-          logOut()
-          
-        }
+    
+      data[indexPath.section][indexPath.row].handler()
+
   }
   
   
--- InstagramApp/Services/FireBase/StoragManager.swift
+++ InstagramApp/Services/FireBase/StoragManager.swift
@@ -23,12 +23,38 @@ struct UserPost {
 
 final class StoragManager {
   
+  enum IGStorageManagerError: Error {
+    case downLoadError
+    
+    
+    var localizedDescription: String {
+      switch self {
+      case .downLoadError:
+        return "Ошибка загрузки данных"
+      
+      }
+    }
+  }
+  
   static let shared = StoragManager()
   
   private let bucket = Storage.storage().reference()
   
   
   public func uploadUserPhotoPost(model: UserPost,complation: @escaping ((Result<URL,Error>) -> Void)) {
+    
+  }
+  
+  public func downLoadImage(reference : String, complation:@escaping ((Result<URL,IGStorageManagerError>) -> Void)) {
+    
+    bucket.child(reference).downloadURL { (url, error) in
+      guard
+        let url = url,
+        error   != nil
+        else {return complation(.failure(.downLoadError)) }
+      
+      complation(.success(url))
+    }
     
   }
 }
